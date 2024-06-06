164bde6fbfa0f2dfe15291079472d1c69377ce9b
Aquamoris <pestryak.nikita.dev@gmail.com>
2024-06-06 15:19:55 +1000

    <feat> Add Header

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,17 @@ import React from 'react';
 import {Route, Routes} from "react-router";
 import Map from "./pages/Map/Map";
 import Algorithm from "./pages/Algorithm/Algorithm";
+import Header from "./components/Header/Header";
 
 const App = () => {
     return (
-        <Routes>
-            <Route path='/' element={<Map />} />
-            <Route path='/algorithm' element={<Algorithm />} />
-        </Routes>
+        <div>
+            <Header />
+            <Routes>
+                <Route path='/' element={<Map />} />
+                <Route path='/algorithm' element={<Algorithm />} />
+            </Routes>
+        </div>
     );
 };
 
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import "./Header.scss";
+import {Button, ButtonGroup} from "@mui/material";
+import {Link, useLocation} from "react-router-dom";
+
+const Header = () => {
+    const route = useLocation();
+
+    const linksData = [
+        {title: 'Карта', url: '/'},
+        {title: 'Алгоритм', url: '/algorithm'},
+    ]
+
+    return (
+        <div className="header">
+            <ButtonGroup>
+                {
+                    linksData.map(link => (
+                        <Link to={link.url} key={link.url}>
+                            <Button variant={route.pathname === link.url ? 'contained' : undefined}>{link.title}</Button>
+                        </Link>
+                    ))
+                }
+            </ButtonGroup>
+        </div>
+    );
+};
+
+export default Header;
