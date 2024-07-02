2b6e9778420490985ab4b6c4b5bc2f2869b7428c
Nikita Sidorov <sidorov_nv@proitr.ru>
2024-07-02 15:30:17 +1000

    reference data

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import {Route, Routes} from "react-router";
 import MapPage from "./pages/Map/MapPage";
 import AlgorithmPage from './pages/Algorithm/AlgorithmPage';
 import Header from "./components/Header/Header";
-import AdditionalDataPage from './pages/AdditionalData/AdditionalDataPage';
+import ReferenceDataPage from './pages/ReferenceData/RefercenceDataPage';
 
 const App = () => {
     return (
@@ -12,7 +12,7 @@ const App = () => {
             <Routes>
                 <Route path='/' element={<MapPage />} />
                 <Route path='/algorithm' element={<AlgorithmPage />} />
-                <Route path='/additional-data' element={<AdditionalDataPage />} />
+                <Route path='/additional-data' element={<ReferenceDataPage />} />
             </Routes>
         </div>
     );
---
+++ src/pages/ReferenceData/RefercenceDataPage.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ReferenceDataPage = () => {
+  return (
+    <div>
+      Справочные данные
+    </div>
+  )
+}
+
+export default ReferenceDataPage
