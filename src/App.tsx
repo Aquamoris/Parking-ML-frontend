0b58a9f7fbf416c036bdf039943bc90072748619
Aquamoris <pestryak.nikita.dev@gmail.com>
2024-07-03 15:08:37 +1000

    MapPin

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import { Route, Routes } from "react-router";
 import MapPage from "./pages/Map/MapPage";
 import AlgorithmPage from './pages/Algorithm/AlgorithmPage';
 import Header from "./components/Header/Header";
-import AdditionalDataPage from './pages/AdditionalData/AdditionalDataPage';
 import ReferenceDataPage from './pages/ReferenceData/RefercenceDataPage';
 import LoginPage from './pages/Login/LoginPage';
 import { AppProvider } from './services/AppContext';
--- src/components/Map/MapImage.tsx
+++ src/components/Map/MapImage.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import styles from './MapImage.module.scss';
+import MapPin from "./MapPin";
+import {Box} from "@mui/material";
 
 const MapImage = () => {
     return (
-        <div className={styles.map}>
-            <div className={styles.map__pointer}>
-                <div className={styles.map__pointer_head}></div>
-                <div className={styles.map__pointer_body}></div>
-            </div>
-        </div>
+        // @ts-ignore
+        <Box width={1200} heigh={700} sx={{ border: '1px solid #007FFF', borderRadius: '12px' }}>
+            <MapPin fill='#2AA06E'/>
+            <MapPin fill='#FF0000'/>
+            <MapPin fill='#FFCC00'/>
+        </Box>
     );
 };
 
