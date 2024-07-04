e2e42f92961edb84727552bea7c62f80351c0d33
Aquamoris <pestryak.nikita.dev@gmail.com>
2024-07-04 17:27:03 +1000

    Map

--- src/components/Map/MapImage.tsx
+++ src/components/Map/MapImage.tsx
@@ -1,16 +1,33 @@
 import React from 'react';
 import styles from './MapImage.module.scss';
 import MapPin from "./MapPin";
-import {Box} from "@mui/material";
 
 const MapImage = () => {
+    const data = [
+        { top: '220px', left: '750px', fill: '#2AA06E' },
+        { top: '900px', left: '900px', fill: '#FF0000' },
+        { top: '820px', left: '1150px', fill: '#FFCC00' }
+    ]
+
     return (
-        // @ts-ignore
-        <Box width={1200} heigh={700} sx={{ border: '1px solid #007FFF', borderRadius: '12px' }}>
-            <MapPin fill='#2AA06E'/>
-            <MapPin fill='#FF0000'/>
-            <MapPin fill='#FFCC00'/>
-        </Box>
+        // // @ts-ignore
+        // <Box width={1200} heigh={700} sx={{ border: '1px solid #007FFF', borderRadius: '12px' }}>
+        //     <img src="/CenterMap.png" alt="Map"/>
+        //     <MapPin fill='#2AA06E'/>
+        //     <MapPin fill='#FF0000'/>
+        //     <MapPin fill='#FFCC00'/>
+        // </Box>
+
+        <div className={styles.map}>
+            <img className={styles.mapImage} src="/CenterMap.png" alt="Map"/>
+            {
+                data.map(e => (
+                    <div className={styles.mapImagePin} style={{top: `${e.top}`, left: `${e.left}`}}>
+                        <MapPin fill={e.fill} />
+                    </div>
+                ))
+            }
+        </div>
     );
 };
 
--- src/pages/Map/MapPage.tsx
+++ src/pages/Map/MapPage.tsx
@@ -1,15 +1,17 @@
-import React from 'react';
+import React, {useState} from 'react';
 import CitySelect from "../../components/Map/CitySelect";
 import MapImage from "../../components/Map/MapImage";
 
 const MapPage = () => {
+    const [area, setArea] = useState('');
+
     return (
         <div>
             <CitySelect
                 label={'Город'}
                 minWidth={120}
                 maxWidth={200}
-                values={['Владивосток', 'Хабаровск', 'Находка']}
+                values={['Район центр', 'Первореченский район']}
             />
             <MapImage />
         </div>
